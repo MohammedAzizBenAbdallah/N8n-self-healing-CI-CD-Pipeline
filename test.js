fab4b602b7648433ca4a4f53a63e26ab93c02955
MohammedAzizBenAbdallah <abdallahaziz2004@gmail.com>
2026-06-22 14:36:18 +0100

    fixed the test script

--- test.js
+++ test.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
-
+const http = require("https")
 // Basic test server setup on port 5000
+
+const options = {
+    hostname: "localhost",
+    port: 5000,
+    path: "/",
+    method: 'GET'
+}
+
+
 app.get('/', (req, res) => {
 res.status(200).send('OK');
 });
 
 const server = app.listen(5000, () => {
-console.log('Smoke test running...');
+    console.log('Smoke test running...');
+    const req = http.get(options, (res) =>{
+        if(res.statusCode === 200){
+            console.log("Smoke Test Passed")
+            process.exit(0);
+        }
+        else{
+            console.error(`Smoke Test failed with error code: ${res.statusCode}`)
+            process.exit(1)
+        }
+    })
+    req.on('error', (e) => {
+        console.error(`Problem with request: ${e.message}`)
+        process.exit(1)
+    })
+    req.end()
 })
